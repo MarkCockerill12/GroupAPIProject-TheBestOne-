12ecde31cdd4200f209f46fb56a8be254d3732ff
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2022-11-28 20:39:23 +0000

    Adding js for API1
    no im not saying anything extra, it is self explanatory, like why uranus jokes are funny

--- API.js
+++ API.js
@@ -1,34 +1,59 @@
- urlAPI1 = ('https://www.omdbapi.com/?i=tt1201607&apikey=178a7aa9')
+// this is da code for uhhhhhhhh api1 
+urlAPI1 = ('http://www.giantbomb.com/api/search/?api_key=f9dc8fa146302bbe5fdbbfdb95ba885c440dc300&format=json&query="among"&resources=game')
 
-
+// this code here is what searches the api
 function search(){
   searchString = document.getElementById("MOVIENAME").value;
-    url = "https://www.omdbapi.com/?s="+searchString+"&apikey=178a7aa9" ;
+    url = "http://www.giantbomb.com/api/search/?api_key=f9dc8fa146302bbe5fdbbfdb95ba885c440dc300&format=json&query="searchString"&resources=game" ;
     alert(urlAPI1)
     fetch(urlAPI1)
     .then((resp) => resp.json())
-    .then(function(movie) {
-      console.log(movie);
-
+    .then(function(element) {
+      console.log(element);
+     
       movie.Search.forEach(element => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
-  
+ 
         const h1 = document.createElement('h1');
-        h1.textContent = element.Title;
+        h1.textContent = element.name;
   
         const p = document.createElement('p');
-  
+        element.description = element.description.substring(0, 300);
+        p.textContent = `${element.description}...`;
+
         container.appendChild(card);
         card.appendChild(h1);
         card.appendChild(p);
-
-        
       });
-      
-        
    }
   )}
 
+// this is what displays on the webpage as you boot it up
+const app = document.getElementById('root');
 
-//BetA CODE DO NOT WORRY BOut it WE ARE GGUUUD
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+fetch(urlAPI1)
+  .then((resp) => resp.json())
+  .then(function(game) {
+    console.log(game)
+    
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = game.name;
+
+      const p = document.createElement('p');
+      game.description = game.description.substring(0, 300);
+      p.textContent = `${game.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+ }
+)
