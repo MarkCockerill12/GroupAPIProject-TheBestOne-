b5f2f865233df1c20ad9a64a854c3fd90b70d4db
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2022-12-05 13:41:17 +0000

    changing API

--- API.js
+++ API.js
@@ -1,12 +1,16 @@
+// const element =  [];
+
+// const game = [];
+
 // this is da code for uhhhhhhhh api1 
-urlAPI1 = ('https://www.giantbomb.com/api/search/?api_key=f9dc8fa146302bbe5fdbbfdb95ba885c440dc300&format=json&query="Dark%20souls"&resources=game')
+urlAPI1 = ('https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&dates=2019-09-01,2019-09-30&platforms=18,1,7')
 
 // this code here is what searches the api
 function search(){
+  //THIS IS THE CODE FOR VIDEO GAME API
   searchString = document.getElementById("GAMENAME").value;
-    url = "https://www.giantbomb.com/api/search/?api_key=f9dc8fa146302bbe5fdbbfdb95ba885c440dc300&format=json&query="+searchString+"&resources=game" ;
-    alert(urlAPI1)
-    fetch(urlAPI1)
+    url = "https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&dates=2019-09-01,2019-09-30&platforms=18,1,7" ;
+    fetch(url)
     .then((resp) => resp.json())
     .then(function(element) {
       console.log(element);
@@ -19,15 +23,46 @@ function search(){
         h1.textContent = element.name;
   
         const p = document.createElement('p');
-        element.description = element.description.substring(0, 300);
-        p.textContent = `${element.description}...`;
+        element.platform = element.platform.substring(0, 300);
+        p.textContent = `${element.platform}...`;
 
         container.appendChild(card);
         card.appendChild(h1);
         card.appendChild(p);
       });
    }
-  )}
+  )
+  //THIS IS THE CODE FOR TMBLUR API
+  const app = document.getElementById('pics');
+
+  const container = document.createElement('div');
+  container.setAttribute('class', 'container');
+  
+  app.appendChild(container);
+  
+urlAPI2= 
+fetch(urlAPI2)
+  .then((resp) => resp.json())
+  .then(function(tumblr) {
+    console.log(tumblr);
+    
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = tumblr.name;
+
+      const p = document.createElement('p');
+      // tumblr.playtime = tumblr.playtime.substring(0, 300); this limits the description - gotta use this with string words
+      p.textContent = `${tumblr.slug}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+ }
+)
+
+}
 
 // this is what displays on the webpage as you boot it up
 const app = document.getElementById('root');
@@ -40,7 +75,7 @@ app.appendChild(container);
 fetch(urlAPI1)
   .then((resp) => resp.json())
   .then(function(game) {
-    console.log(game)
+    console.log(game);
     
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
@@ -49,8 +84,8 @@ fetch(urlAPI1)
       h1.textContent = game.name;
 
       const p = document.createElement('p');
-      game.description = game.description.substring(0, 300);
-      p.textContent = `${game.description}...`;
+      // game.playtime = game.playtime.substring(0, 300); this limits the description - gotta use this with string words
+      p.textContent = `${game.name}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
