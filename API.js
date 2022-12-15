006ebc5539aefa259514d63142829ec3c1100c44
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2022-12-15 03:34:30 +0000

    Updated Initial Design

--- API.js
+++ API.js
@@ -1,6 +1,10 @@
-// const element =  [];
+const app = document.getElementById('root');
 
-// const game = [];
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
 
 // this is da code for uhhhhhhhh api1 
 urlAPI1 = ('https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&dates=2019-09-01,2019-09-30&platforms=18,1,7')
@@ -9,90 +13,62 @@ urlAPI1 = ('https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&d
 function search(){
   //THIS IS THE CODE FOR VIDEO GAME API
   searchString = document.getElementById("GAMENAME").value;
-    url = "https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&dates=2019-09-01,2019-09-30&platforms=18,1,7" ;
+    url = "https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&search="+searchString ;
     fetch(url)
     .then((resp) => resp.json())
     .then(function(element) {
       console.log(element);
+      for (i in element.results){
+        console.log(element.results[i].name);
      
-      element.Search.forEach(element => {
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
  
         const h1 = document.createElement('h1');
-        h1.textContent = element.name;
-  
-        const p = document.createElement('p');
-        element.platform = element.platform.substring(0, 300);
-        p.textContent = `${element.platform}...`;
+        h1.textContent = element.results[i].name;
+
+      const p = document.createElement('p');
+      // element.playtime = element.playtime.substring(0, 300); this limits the description - gotta use this with string words
+      p.textContent = `${element.results[i].playtime}`;
 
         container.appendChild(card);
         card.appendChild(h1);
         card.appendChild(p);
-      });
+      }
    }
   )
+  
   //THIS IS THE CODE FOR TMBLUR API
-  const app = document.getElementById('pics');
+//   const app = document.getElementById('pics');
 
-  const container = document.createElement('div');
-  container.setAttribute('class', 'container');
+//   const container = document.createElement('div');
+//   container.setAttribute('class', 'container');
   
-  app.appendChild(container);
-  
-urlAPI2= 
-fetch(urlAPI2)
-  .then((resp) => resp.json())
-  .then(function(tumblr) {
-    console.log(tumblr);
+//   app.appendChild(container);
+
+// urlAPI2= 
+// fetch(urlAPI2)
+//   .then((resp) => resp.json())
+//   .then(function(tumblr) {
+//     console.log(tumblr);
     
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+//       const card = document.createElement('div');
+//       card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1');
-      h1.textContent = tumblr.name;
+//       const h1 = document.createElement('h1');
+//       h1.textContent = tumblr.name;
 
-      const p = document.createElement('p');
-      // tumblr.playtime = tumblr.playtime.substring(0, 300); this limits the description - gotta use this with string words
-      p.textContent = `${tumblr.slug}...`;
+//       const p = document.createElement('p');
+//       // tumblr.playtime = tumblr.playtime.substring(0, 300); this limits the description - gotta use this with string words
+//       p.textContent = `${tumblr.slug}...`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
- }
-)
+//       container.appendChild(card);
+//       card.appendChild(h1);
+//       card.appendChild(p);
+//  }
+// )
 
 }
 
-// this is what displays on the webpage as you boot it up
-const app = document.getElementById('root');
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
 
-app.appendChild(container);
-
-fetch(urlAPI1)
-  .then((resp) => resp.json())
-  .then(function(game) {
-    console.log(game);
-    for (i in game.results){
-      console.log(game.results[i].name)
-    
-    
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-
-      const h1 = document.createElement('h1');
-      h1.textContent = game.results[i].name;
-
-      const p = document.createElement('p');
-      // game.playtime = game.playtime.substring(0, 300); this limits the description - gotta use this with string words
-      p.textContent = `${game.results[i].released}`; //add ... after } for description effect
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    }
- }
-)
