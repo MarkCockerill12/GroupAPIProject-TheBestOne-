f85772aafa525e116532fac74a38f4719fec9b87
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2022-12-23 04:26:43 +0000

    update

--- API.js
+++ API.js
@@ -1,9 +1,4 @@
 
-//  gameID= "1";
-//  urlMoreInfo ="";
-let searchString= "yes";
-
-
 // this code here is what searches the api
 function search(){
 
@@ -14,14 +9,16 @@ container.setAttribute('class', 'container');
 
 app.appendChild(container);
 
+var gameID= "";
   
   //THIS IS THE CODE FOR VIDEO GAME API
   searchString = document.getElementById("GAMENAME").value;
-    url = "https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&search="+searchString+ "&page_size=18";
+    url = "https://api.rawg.io/api/games?key=223cbd9b75454545a8743e05c64645ff&search="+searchString+ "&page_size=9";
     fetch(url)
     .then((resp) => resp.json())
     .then(function(element) {
       console.log(element);
+
       for (i in element.results){
         console.log(element.results[i].name);
 
@@ -30,97 +27,90 @@ app.appendChild(container);
  
         const h1 = document.createElement('h1');
         h1.textContent = element.results[i].name;
-        h1.classList.add("mt-3");
 
        const img = document.createElement('img');
        img.src = element.results[i].background_image;
         img.style.height = '250px'
         img.style.width = '350px'
 
-       const cardButton = document.createElement('a');
-
-       cardButton.innerHTML = ("More Info");
 
         container.appendChild(card);
         card.appendChild(h1);
         card.appendChild(img);
-        card.appendChild(cardButton);
-      }
-   }
+        } 
+
+        gameID= element.results[0].id;
+        console.log(gameID)
+        displayInfo(searchString, gameID)
+   } 
   )
 }
 
 
 
 
-  function displayInfo(){
+ function displayInfo(searchString, gameID){
 
 
-  const app = document.getElementById('info');
+    const app = document.getElementById('info');
 
-  const container = document.createElement('div');
-  container.setAttribute('class', 'container');
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    
+    app.appendChild(container);
 
-  app.appendChild(container);
+   urlMoreInfo= "https://api.rawg.io/api/games/"+gameID+"?key=223cbd9b75454545a8743e05c64645ff&page_size=1";
 
-   //urlMoreInfo= "https://api.rawg.io/api/games/"+ gameID+ "?key=223cbd9b75454545a8743e05c64645ff&page_size=1";
+  fetch(urlMoreInfo)
+    .then((resp) => resp.json())
+    .then(function(element) {
+      console.log(element);
+        console.log(element.name);
 
-  // fetch(urlMoreInfo)
-  //   .then((resp) => resp.json())
-  //   .then(function(element) {
-  //     console.log(element);
-  //     for (i in element.results){
-  //       console.log(element.results[i].name);
-
-  //       const card = document.createElement('div');
-  //       card.setAttribute('class', 'card');
+        const card = document.createElement('card');
+         card.setAttribute('class', 'card1');
  
-  //       const h1 = document.createElement('h1');
-  //       h1.textContent = element.results[i].name;
+        const h3 = document.createElement('h3');
+        h3.textContent = element.name;
 
-  //      const img = document.createElement('img');
-  //       img.src = element.results[i].background_image;
-  //       img.style.height = '350px'
-  //       img.style.width = '450px'
+        const p = document.createElement('p');
+        p.textContent = element.description_raw;
 
-  //       const p = document.createElement('img');
-  //       p.textContent = element.results[i].description;
+        container.appendChild(card);
+        card.appendChild(h3);
+        card.appendChild(p);
 
-  //       container.appendChild(card);
-  //       card.appendChild(h1);
-  //       card.appendChild(img);
-  //       card.appendChild(p);
-
-  //     }})
-
-  //  console.log(searchString)
+      })
 
 
 
 //THIS IS THE CODE FOR deviant API
- searchString="dark souls" //THIS IS A TEMPORARY THING FOR TESTING, WE WANNA MATCH THE STRING TO THE PREVIOUS ONE
 
-fetch('https://backend.deviantart.com/rss.xml?type=deviation&q=boost%3Apopular+in%3Adigitalart%2Fdrawings+'+searchString)
-// .then((resp) => resp.json())
-// 
-  .then(response => response.text())
-  .then(function(art) {
-    console.log(art);
-    for (i in art.media){
-   console.log(art.media[i].title)
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
+ 
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'ff335927f9msh39686095c4a23c5p1d8c91jsn4609590eb49a',
+        'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q='+searchString+'%20fan%20art&pageNumber=1&pageSize=18&autoCorrect=true', options)
+    .then((resp) => resp.json())
+    .then(function(art) {
+      console.log(art);
+      for (i in art.value){
 
-    const h1 = document.createElement('h1');
-    h1.textContent = art.media[i].title;
-
-    const img = document.createElement('img');
-    img.src = `${art.media[i].content}`;
-
-    container.appendChild(card);
-    card.appendChild(h1);
-    card.appendChild(img);
-
-     }})
-
-     }
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card2');
+    
+        const img = document.createElement('img');
+        img.src = `${art.value[i].url}`;
+    
+        container.appendChild(card);
+        card.appendChild(img);
+    
+         }   
+         })
+  
+  }
